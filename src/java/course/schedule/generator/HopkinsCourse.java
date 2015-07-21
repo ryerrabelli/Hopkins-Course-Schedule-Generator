f6d8592f3c8ba4a330e4a87bbe35067123de8e93
Rahul <Yerrabelli>
2015-07-21 15:23:33 -0400

    Added courseTitle, credits, and area constructor for course

--- src/java/course/schedule/generator/HopkinsCourse.java
+++ src/java/course/schedule/generator/HopkinsCourse.java
@@ -9,5 +9,26 @@ package java.course.schedule.generator;
  * @author ryerrabelli
  */
 public class HopkinsCourse {
+    //  school.deptNum.courseNum
+    //         AS.030.205
+    private String deptNum = "";
+    private String courseNum = "";
+    private boolean isWhiting;
+    
+    private int credits = 0;
+    private String area = ""; 
+    
+    public HopkinsCourse(String courseTitle, int credits, String area) {
+        this.credits = credits;
+        this.area = area.toUpperCase().trim();
+        String[] courseParts = courseTitle.split("\\Q.\\E", 3);
+        if (courseParts.length == 3) {
+            if (courseParts[0].trim().equalsIgnoreCase("EN")) isWhiting = true;
+            else isWhiting = false;
+            deptNum = courseParts[1].trim();
+            courseNum = courseParts[2].trim();
+        }
+        
+    }
     
 }
