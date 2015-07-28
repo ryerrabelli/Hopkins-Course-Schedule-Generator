0c71024c82e2d1e9bdacf94e48264610a2c236de
rahulyerrabelli <rahulyerrabelli@rahulyelisimac2>
2015-07-27 20:21:46 -0400

    GenericCourse is no longer a superclass of HopkinsClass. They are both now a subclass of Course

--- src/course/schedule/machine/GenericCourse.java
+++ src/course/schedule/machine/GenericCourse.java
@@ -10,53 +10,21 @@ import java.util.Set;
  *
  * @author rahulyerrabelli
  */
-public class GenericCourse implements Requirable  {
-    protected String deptNum = "";
-    protected String courseNum = "";
-    protected float credits = 0;
-    protected int school = 0; //0 - not determined 1-krieger 2-whiting 3- Peabody 4-other jhu   5-transfer
-    protected Boolean isDesign = null;
-    protected Boolean isWritingIntensive = null;
-    protected String area = ""; 
+public class GenericCourse extends Course {
+
     int identificationNum;
     
     public GenericCourse(String courseTitle, float credits, String area){
-        this.credits = credits;
-        this.area = area.toUpperCase().trim();
-        String[] courseParts = courseTitle.split("\\Q.\\E", 3);
-        if (courseParts.length == 3) {
-            courseParts[0] = courseParts[0].trim().toUpperCase();
-            switch (courseParts[0]) {
-                case "AS": school = 1;
-                case "EN": school = 2;
-                case "PP":
-                case "PR": school = 3;
-                case "TR": school = 5;
-                default: school = 4;
-            }
-            /*Before java 1.7 if (courseParts[0].equals("AS")) school = 1;
-            else if (courseParts[0].equals("EN")) school = 2;
-            else if (courseParts[0].equals("PP") || courseParts[0].equals("PY")) school = 3;
-            else if (courseParts[0].equals("TR")) school = 5;
-            else school = 4; */
-            deptNum = courseParts[1].trim();
-            courseNum = courseParts[2].trim();
-        } else if (courseParts.length == 2) {
-            deptNum = courseParts[0].trim();
-            courseNum = courseParts[1].trim();
-            try {
-                school = Integer.parseInt(deptNum) >= 500 ? 2 : 1;
-            } catch (NumberFormatException ex) { System.out.println("Error: deptNum is not a number");}
-        } else System.out.println("Course title does not have enough parts: " + courseTitle);
+        super(courseTitle, credits, area);
     }
     
-    private boolean isCompatibleWith(GenericCourse otherCourse) {
+    private boolean isCompatibleWith(GenericCourse otherCourse) { 
         if (!this.deptNum.isEmpty() && !otherCourse.deptNum.isEmpty() && !this.deptNum.equals(otherCourse.deptNum)) return false;
         if (courseNum.length()>=3  && otherCourse.courseNum.length()>=3 && !this.courseNum.equals(otherCourse.courseNum)) return false;
         return true;
     }
     
-    private boolean isSatisfiedBy(HopkinsCourse realCourse) {
+    boolean isSatisfiedBy(HopkinsCourse realCourse) {
         try {
             if (deptNum.isEmpty() || !deptNum.matches(realCourse.deptNum)) return false;
             if (courseNum.length() == 3 && !courseNum.equalsIgnoreCase(realCourse.getCourseNum())) return false;
@@ -78,6 +46,17 @@ public class GenericCourse implements Requirable  {
             if (this.isSatisfiedBy(course)) return true;
         }
         return false;
+    }
+    
+    public boolean isSubsetOf(GenericCourse genCourse) { //returns whether this GenericCourse can fit inside the input
+        if (!deptNum.isEmpty() && !deptNum.equals(genCourse.deptNum)) return false;
+        if (!courseNum.isEmpty()) {
+            if (courseNum.length() >= 3 && !courseNum.equalsIgnoreCase(genCourse.courseNum)) return false;
+            if (Integer.valueOf(courseNum.charAt(0)) > Integer.valueOf(genCourse.courseNum.charAt(0))) return false;
+        }
+        if (this.credits > genCourse.credits) return false;
+        if (school > 0 && school != genCourse.school) return false;
+        return true;
     }
     
 }
