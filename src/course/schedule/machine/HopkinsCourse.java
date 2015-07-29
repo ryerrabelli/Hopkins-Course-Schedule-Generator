4afaca2053009503b0adcf5de6304696242596c9
rahulyerrabelli <rahulyerrabelli@rahulyelisimac2>
2015-07-29 14:05:26 -0400

    Moved Course constructor to HopkinsCourse Added tags

--- src/course/schedule/machine/HopkinsCourse.java
+++ src/course/schedule/machine/HopkinsCourse.java
@@ -6,6 +6,7 @@ package course.schedule.machine;
 import course.schedule.machine.HopkinsClass.Semester;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 /**
  *
@@ -23,11 +24,47 @@ public class HopkinsCourse extends Course {
     protected boolean isLab = false;
     protected float priority = 0f;
     protected static HashMap<String, HopkinsCourse> allCourses = new HashMap<String, HopkinsCourse>();
+    protected static HashMap<String, HashSet<HopkinsCourse>> tagMap = new HashMap<String, HashSet<HopkinsCourse>>();
     
     public HopkinsCourse(String courseTitle, String verbalName, String area, boolean isWritingIntensive, boolean isDesign, boolean isLab, float creditsWorth, Semester semester, int year) {
-        super(courseTitle, creditsWorth, area);
+        this.credits = credits;
+        this.area = area.toUpperCase().trim();
+        String[] courseParts = courseTitle.split("\\Q.\\E", 3);
+        if (courseParts.length == 3) {
+            courseParts[0] = courseParts[0].trim().toUpperCase();
+            if (courseParts[0].length() == 0) school = 0;
+            else switch (courseParts[0]) {
+                    case "AS": school = 1;
+                    case "EN": school = 2;
+                    case "PP":
+                    case "PR": school = 3;
+                    case "TR": school = 5;
+                    default: school = 4;
+                }
+            /*Before java 1.7 if (courseParts[0].equals("AS")) school = 1;
+            else if (courseParts[0].equals("EN")) school = 2;
+            else if (courseParts[0].equals("PP") || courseParts[0].equals("PY")) school = 3;
+            else if (courseParts[0].equals("TR")) school = 5;
+            else school = 4; */
+            deptNum = courseParts[1].trim();
+            courseNum = courseParts[2].trim();
+        } else if (courseParts.length == 2) {
+            deptNum = courseParts[0].trim();
+            courseNum = courseParts[1].trim();
+            try {
+                school = Integer.parseInt(deptNum) >= 500 ? 2 : 1;
+            } catch (NumberFormatException ex) { System.out.println("Error: deptNum is not a number");}
+        } else System.out.println("Course title does not have enough parts: " + courseTitle);
         // MUST FINISH LATER
         
+    }
+    
+    public static void createNewTag(String tagName, HashSet<HopkinsCourse> tagElements) {
+        tagMap.put(tagName, tagElements);
+    }
+    public static HashSet<HopkinsCourse> getCoursesWithTag(String tagName) {
+        HashSet<HopkinsCourse> toReturn = tagMap.get(tagName);
+        return toReturn == null ? new HashSet<HopkinsCourse>() : toReturn;
     }
     
     /*public HopkinsCourse(String courseTitle, int credits, String area, boolean isDesign, boolean isWritingIntensive){
